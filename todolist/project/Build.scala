1ed0ea5e1cea1943f9803105a98cd3ba4b369052
Takashi Yoshioka <not.for.official.use.9@gmail.com>
2014-07-17 01:28:07 +0900

    build.scala changed

--- todolist/project/Build.scala
+++ todolist/project/Build.scala
@@ -1,3 +1,8 @@
-val appDependencies = Seq(
-  "postgresql" % "postgresql" % "8.4-702.jdbc4"
-)
+import sbt._
+import Keys._
+
+object ApplicationBuild extends Build {
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
+}
